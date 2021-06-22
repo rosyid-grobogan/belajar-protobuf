c1adeb173f5cf4ef618dfa4ca369404f699ca4a4
Rosyid <rosyidgrobogan@gmail.com>
2021-06-22 20:15:57 +0700

    + Person builder Pattern Demo

---
+++ src/main/java/com/rosyidgrobogan/handsonprotobuf/PersonDemo.java
@@ -0,0 +1,15 @@
+package com.rosyidgrobogan.handsonprotobuf;
+
+
+
+import com.rosyidgrobogan.api.Person;
+
+public class PersonDemo {
+
+    public static void main(String[] args) {
+        Person rosyid = Person.newBuilder()
+                .setName("Rosyid")
+                .setAge(25)
+                .build();
+    }
+}
